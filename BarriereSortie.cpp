e9d8b9c879501736102dc9d1a1704284d1acb3c4
Liermann Jerome <jliermann@if501-219-10.insa-lyon.fr>
2016-04-05 23:10:22 +0200

    gestion plusieurs sorties

--- BarriereSortie.cpp
+++ BarriereSortie.cpp
@@ -21,9 +21,12 @@ using namespace std;
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/sem.h>
+#include <time.h>
 //------------------------------------------------------ Include personnel
 
 #include "Config.h"
+
+#include <cstdio>
 
 //------------------------------------------------------------- Constantes
 
@@ -47,7 +50,116 @@ static int semCompteur;
 static int semPark;
 static int semReq[3];
 static int idAuto[3];
-static int currentSortie=0;
+
+static int pid_voiturier[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+void end(int noSignal);
+void run(int idSortieVoiture);
+void finNormale(int noSignal);
+void checkAutorisations();
+void init(int idParking, int idCpt, int semCpt, int semParking, int idRequete[], int semRequete[], int idAutorisation[]);
+
+
+void BarriereSortie(int idParking, int idRequete[], int idAutorisation[], int idSortieVoiture, int semParking, int semRequete[], int idCpt, int semCpt)
+{
+	//init
+	init(idParking, idCpt, semCpt, semParking, idRequete, semRequete, idAutorisation);
+
+	run(idSortieVoiture);
+
+	// detachement	
+	shmdt(parking);
+	shmdt(compteur);
+	for(int i=0; i<3; i++)
+	{
+		shmdt(requete[i]);
+	}
+}
+
+
+//------------------------------------------------------------------ PRIVE
+
+void end(int noSignal)
+{
+
+	for(int i=0; i<8; i++)
+	{
+		if(pid_voiturier[i] != 0)
+		{
+			kill(pid_voiturier[i], SIGUSR2);
+			waitpid(pid_voiturier[i], NULL, 0);
+		}
+	}
+	
+	// detachement	
+	shmdt(parking);
+	shmdt(compteur);
+	for(int i=0; i<3; i++)
+	{
+		shmdt(requete[i]);
+	}
+
+	exit(0);
+}
+
+void run(int idSortieVoiture)
+{
+	while(true)
+	{
+		msgBaL * lecture = new msgBaL();
+		msgrcv(idSortieVoiture, lecture, sizeof(int), 1, 0);
+		pid_voiturier[lecture->place[0]-1] = SortirVoiture(lecture->place[0]);
+		delete lecture;	
+	}
+}
+
+void finNormale(int noSignal)
+{
+	int place;
+	waitpid(0, &place, 0);
+	place=WEXITSTATUS(place);
+
+	Voiture v = parking[place-1];
+	v.hSortie = time(NULL);
+	AfficherSortie(v.type, v.num, v.hEntree, v.hSortie);
+
+	while(semop(semCompteur, &reserver, 1)==-1);
+	(*compteur)++;
+	while(semop(semCompteur, &liberer, 1)==-1);
+
+	switch(place-1)
+	{
+		case 0:
+			Effacer(ETAT_P1);
+			break;
+		case 1:
+			Effacer(ETAT_P2);
+			break;
+		case 2:
+			Effacer(ETAT_P3);
+			break;
+		case 3:
+			Effacer(ETAT_P4);
+			break;
+		case 4:
+			Effacer(ETAT_P5);
+			break;
+		case 5:
+			Effacer(ETAT_P6);
+			break;
+		case 6:
+			Effacer(ETAT_P7);
+			break;
+		case 7:
+			Effacer(ETAT_P8);
+			break;
+	}
+
+	pid_voiturier[place-1]=0;
+	checkAutorisations();
+	
+}
+
 
 void checkAutorisations()
 {
@@ -74,10 +186,28 @@ void checkAutorisations()
 							nChoisi = i;
 						}
 					}
+					else
+					{
+						if(requete[nChoisi]->type==AUTRE && requete[i]->hRequete < requete[nChoisi]->hRequete)
+						{
+							nChoisi = i;
+						}
+					}
 				}
 			}
-
 			requete[nChoisi]=NULL;
+			switch(nChoisi)
+			{
+				case 0:
+					Efface(REQUETE_R1);
+					break;
+				case 1:
+					Efface(REQUETE_R2);
+					break;
+				case 2:
+					Efface(REQUETE_R3);
+					break;
+			}
 			while(semop(idAuto[nChoisi], &liberer, 1)==-1);
 			
 		}
@@ -87,56 +217,8 @@ void checkAutorisations()
 	}
 }
 
-void finNormale(int noPlace)
+void init(int idParking, int idCpt, int semCpt, int semParking, int idRequete[], int semRequete[], int idAutorisation[])
 {
-	Voiture v = parking[noPlace];
-	AfficherSortie(v.type, v.num, v.hEntree, v.hSortie);
-	while(semop(semCompteur, &reserver, 1)==-1);
-	(*compteur)++;
-	while(semop(semCompteur, &liberer, 1)==-1);
-	checkAutorisations();
-	
-}
-
-void run(int idSortieVoiture)
-{
-	while(true)
-	{
-		msgBaL * lecture = new msgBaL();
-		msgrcv(idSortieVoiture, lecture, sizeof(int), 1, 0);
-		currentSortie = SortirVoiture(lecture->place[0]);
-		int place=lecture->place[0];	
-		//waitpid(currentSortie, &place, 0);
-		finNormale(place);
-		currentSortie=0;
-		delete lecture;	
-	}
-}
-
-void end(int noSignal)
-{
-	if(currentSortie != 0)
-	{
-		kill(currentSortie, SIGUSR2);
-		waitpid(currentSortie, NULL, 0);
-	}
-	
-	// detachement	
-	shmdt(parking);
-	shmdt(compteur);
-	for(int i=0; i<3; i++)
-	{
-		shmdt(requete[i]);
-	}
-
-	exit(0);
-}
-
-
-
-void BarriereSortie(int idParking, int idRequete[], int idAutorisation[], int idSortieVoiture, int semParking, int semRequete[], int idCpt, int semCpt)
-{
-	//init
 	parking = (Voiture*) shmat(idParking, NULL, 0);
 	compteur = (int *) shmat(idCpt, NULL, 0);
 
@@ -150,12 +232,12 @@ void BarriereSortie(int idParking, int idRequete[], int idAutorisation[], int id
 	termine.sa_flags=0;
 	sigaction(SIGUSR2, &termine, NULL);
 
-	/*//handler fin normale
-	struct sigaction finSortie;
-	finSortie.sa_handler = finNormale;
-	sigemptyset(&finSortie.sa_mask);
-	finSortie.sa_flags=0;
-	sigaction(SIGCHLD, &finSortie, NULL);*/
+	//handler finVoiturier
+	struct sigaction finVoiturier;
+	finVoiturier.sa_handler = finNormale;
+	sigemptyset(&finVoiturier.sa_mask);
+	finVoiturier.sa_flags=0;
+	sigaction(SIGCHLD, &finVoiturier, NULL);
 
 	for(int i=0; i<3; i++)
 	{
@@ -163,20 +245,8 @@ void BarriereSortie(int idParking, int idRequete[], int idAutorisation[], int id
 		semReq[i] = semRequete[i];
 		idAuto[i] = idAutorisation[i];
 	}
-
-	run(idSortieVoiture);
-
-	// detachement	
-	shmdt(parking);
-	shmdt(compteur);
-	for(int i=0; i<3; i++)
-	{
-		shmdt(requete[i]);
-	}
 }
 
-
-//------------------------------------------------------------------ PRIVE
 
 //----------------------------------------------------- Méthodes protégées
 
